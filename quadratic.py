841fb552f45f964d9cc9042d5d3aa00b2ec90ce6
vigneshwaran2646 <61581248+vigneshwaran2646@users.noreply.github.com>
2020-02-28 15:46:23 +0530

    Add files via upload

---
+++ quadratic.py
@@ -0,0 +1,9 @@
+import cmath
+a = int(input(' enter a new number: '))
+b = int(input(' enter a new number: '))
+c = int(input(' enter a new number: '))
+
+d = (b ** 2) - (4 * a * c)
+sol1 = (-b-cmatch.sqrt(d)) / (2 * a)
+sol2 = (-b + cmatch.sqrt(d))/(2 * a)
+print('the solution are {0}and{1}'.format(sol1,sol2))
---
+++ script.py
@@ -0,0 +1,5 @@
+a ={5,2,3,1,4}
+
+print("a =",a)
+
+print(type(a))
